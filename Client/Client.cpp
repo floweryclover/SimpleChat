62752db22bf71e49975b35bdd4a802a1775f7aff
floweryclover <floweryclover@gmail.com>
2023-08-28 21:27:35 +0900

    TCP 연결 구현

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,7 +1,42 @@
 ﻿#include <iostream>
 
-int main()
+#include "boost/asio.hpp"
+#include "boost/bind/bind.hpp"
+#include "boost/array.hpp"
+
+int main(int argc, char* argv[])
 {
-    //ㅇㅇㅇ
-    std::cout << "Hello World!\n";
+	if (argc != 4)
+	{
+		std::cout << "usage: client <server-ip> <port> <nickname>" << std::endl;
+		return 1;
+	}
+	
+	try
+	{
+		boost::asio::io_context io_context;
+		boost::asio::ip::tcp::socket socket(io_context);
+		boost::asio::ip::tcp::endpoint localhost_endpoint(boost::asio::ip::address::from_string(argv[1]), std::atoi(argv[2]));
+		socket.connect(localhost_endpoint);
+		for (;;)
+		{
+			boost::array<char, 128> buf;
+			boost::system::error_code error;
+			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+			if (error == boost::asio::error::eof)
+				break;
+			else if (error)
+				throw boost::system::system_error(error);
+
+			std::cout.write(buf.data(), len);
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	system("pause");
+	
+	return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,13 +1,57 @@
 ﻿#include <iostream>
+#include <memory>
+
 #include "boost/asio.hpp"
-#include "boost/date_time/posix_time/posix_time.hpp"
+#include "boost/bind/bind.hpp"
+
+class ChatServer
+{
+public:
+	ChatServer(boost::asio::io_context& io_context) :
+		io_context_(io_context),
+		acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 31414))
+	{
+		WaitForAccept();
+	}
+private:
+	void WaitForAccept()
+	{
+		std::shared_ptr<boost::asio::ip::tcp::socket> remote_socket = std::make_shared<boost::asio::ip::tcp::socket>(boost::asio::ip::tcp::socket(io_context_));
+		std::shared_ptr<boost::asio::ip::tcp::endpoint> remote_endpoint = std::make_shared<boost::asio::ip::tcp::endpoint>(boost::asio::ip::tcp::endpoint());
+		acceptor_.async_accept(*remote_socket, *remote_endpoint, boost::bind(&ChatServer::AcceptHandler, this, boost::asio::placeholders::error, remote_socket, remote_endpoint));
+	}
+
+	void AcceptHandler(const boost::system::error_code& error, std::shared_ptr<boost::asio::ip::tcp::socket> remote_socket, std::shared_ptr<boost::asio::ip::tcp::endpoint> remote_endpoint)
+	{
+		if (!error)
+		{
+			std::cout << *remote_endpoint << std::endl;
+			WaitForAccept();
+		}
+		else
+		{
+			std::cerr << error << std::endl;
+			std::cerr << "An error occured" << std::endl;
+			return;
+		}
+	}
+
+	boost::asio::io_context& io_context_;
+	boost::asio::ip::tcp::acceptor acceptor_;
+	
+};
 
 int main()
 {
-    boost::asio::io_service io;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-    t.wait();
-    std::cout << "Hello World!\n";
-    system("pause");
-    return 0;
+	try
+	{
+		boost::asio::io_context io_context;
+		ChatServer chat_server(io_context);
+		io_context.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
