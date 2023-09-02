3e36b0233e1166bc6130f600b542e23bb4c843b4
floweryclover <floweryclover@gmail.com>
2023-09-02 22:59:24 +0900

    인증키 입력까지 구현

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <thread>
 
 #include "boost/asio.hpp"
 #include "boost/bind/bind.hpp"
@@ -8,9 +9,9 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc != 4)
+	if (argc != 3)
 	{
-		std::cout << "usage: client <server-ip> <port> <nickname>" << std::endl;
+		std::cout << "usage: client <server-ip> <port>" << std::endl;
 		return 1;
 	}
 	
@@ -20,49 +21,63 @@ int main(int argc, char* argv[])
 		boost::asio::ip::tcp::socket socket(io_context);
 		boost::asio::ip::tcp::endpoint localhost_endpoint(boost::asio::ip::address::from_string(argv[1]), std::atoi(argv[2]));
 		socket.connect(localhost_endpoint);
-		for (;;)
+
+
+		std::string body;
+		std::cout << "서버 운영자가 설정한 인증키를 입력하세요: ";
+		std::cin >> body;
+		size_t header = body.size();
+		std::vector<char> send_buffer(sizeof(header) + body.size());
+		std::memcpy(send_buffer.data(), &header, sizeof(header));
+		std::memcpy(send_buffer.data()+sizeof(header), body.data(), body.size());
+		boost::system::error_code error;
+		socket.send(boost::asio::buffer(send_buffer));
+
+		std::array<char, 1> auth_response_buf = {};
+		size_t len = socket.read_some(boost::asio::buffer(auth_response_buf), error);
+
+		if (error == boost::asio::error::eof)
 		{
-			boost::array<char, 128> buf;
-			//
-			std::string body("메세지1");
-			size_t header = body.size();
-			std::vector<char> send_buffer(sizeof(header) + body.size());
-			std::memcpy(send_buffer.data(), &header, sizeof(header));
-			std::memcpy(send_buffer.data()+sizeof(header), body.data(), body.size());
-			boost::system::error_code error;
-			socket.send(boost::asio::buffer(send_buffer));
-
-			body = "두번째 메세지";
-			header = body.size();
-			send_buffer.resize(sizeof(header) + body.size());
-			std::memcpy(send_buffer.data(), &header, sizeof(header));
-			std::memcpy(send_buffer.data() + sizeof(header), body.data(), body.size());
-			socket.send(boost::asio::buffer(send_buffer));
-
-			body = "Third message";
-			header = body.size();
-			send_buffer.resize(sizeof(header) + body.size());
-			std::memcpy(send_buffer.data(), &header, sizeof(header));
-			std::memcpy(send_buffer.data() + sizeof(header), body.data(), body.size());
-			socket.send(boost::asio::buffer(send_buffer));
-
-			body = "A long long long long long long long long long long long long long long long long long long long long long long long long message";
-			header = body.size();
-			send_buffer.resize(sizeof(header) + body.size());
-			std::memcpy(send_buffer.data(), &header, sizeof(header));
-			std::memcpy(send_buffer.data() + sizeof(header), body.data(), body.size());
-			socket.send(boost::asio::buffer(send_buffer));
-
-
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			if (error == boost::asio::error::eof)
-				break;
-			else if (error)
-				throw boost::system::system_error(error);
-
-			std::cout.write(buf.data(), len);
+			std::cerr << "서버와의 연결이 끊겼습니다." << std::endl;
+			return 1;
 		}
+		else if (error)
+			throw boost::system::system_error(error);
+
+		if (*auth_response_buf.begin() == '0')
+		{
+			std::cerr << "인증키가 올바르지 않습니다." << std::endl;
+			system("pause");
+			return 1;
+		}
+
+		std::cout << "닉네임을 설정하세요: ";
+		std::cin >> body;
+		header = body.size();
+		send_buffer.resize(sizeof(header) + body.size());
+		std::memcpy(send_buffer.data(), &header, sizeof(header));
+		std::memcpy(send_buffer.data() + sizeof(header), body.data(), body.size());
+		socket.send(boost::asio::buffer(send_buffer));
+
+		std::cout << "이제 채팅을 시작하세요." << std::endl;
+
+		std::thread input_thread = std::thread([&socket]()
+			{
+				std::string message;
+				size_t header;
+				std::vector<char> send_buffer;
+				while (true)
+				{
+					std::cin >> message;
+					header = message.size();
+					send_buffer.resize(sizeof(header) + message.size());
+					std::memcpy(send_buffer.data(), &header, sizeof(header));
+					std::memcpy(send_buffer.data() + sizeof(header), message.data(), message.size());
+					socket.send(boost::asio::buffer(send_buffer));
+				}
+			});
+		input_thread.join();
+
 	}
 	catch (std::exception& e)
 	{
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -3,21 +3,42 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <functional>
 
 #include "boost/asio.hpp"
 #include "boost/bind/bind.hpp"
 
+enum ClientOperation
+{
+	Unauthorized,
+	EnteringNickname,
+	Online,
+	Disconnected,
+};
 
 class TcpConnection
 {
 public:
-	TcpConnection(boost::asio::io_context& io_context) : socket_(io_context)
+	TcpConnection(
+		boost::asio::io_context& io_context,
+		const std::string& passkey,
+		std::function<void(const std::string&, const std::string&)> fn_on_chat,
+		std::function<void(const std::string&)> fn_on_disconnected) noexcept :
+
+		socket_(io_context),
+		header_({}),
+		receive_buffer_({}),
+		OnClientChat_(fn_on_chat),
+		OnClientDisconnected_(fn_on_disconnected),
+		nickname_("Not logged in"),
+		current_operation_(ClientOperation::Unauthorized),
+		passkey_(passkey)
 	{
 
 	}
 
-	boost::asio::ip::tcp::socket& Socket() { return this->socket_; }
-	boost::asio::ip::tcp::endpoint& Endpoint() { return this->endpoint_; }
+	boost::asio::ip::tcp::socket& Socket() noexcept { return this->socket_; }
+	boost::asio::ip::tcp::endpoint& Endpoint() noexcept { return this->endpoint_; }
 
 	void StartReceive()
 	{
@@ -25,13 +46,16 @@ public:
 		socket_.async_receive(boost::asio::buffer(receive_buffer_, header_to_receive_), boost::bind(&TcpConnection::ReceiveHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 	}
 
-private:
+	bool IsDisconnected() const noexcept { return this->current_operation_ == Disconnected; }
 
+private:
 	void ReceiveHandler(const boost::system::error_code& error, size_t bytes_transferred)
 	{
 		if (error)
 		{
-			std::cerr << "An error occured: " << error << std::endl;
+			std::cerr << "[종료 코드] " << nickname_ << ": " << error << std::endl;
+			this->current_operation_ = Disconnected;
+			OnClientDisconnected_(nickname_);
 			return;
 		}
 
@@ -60,13 +84,13 @@ private:
 			std::copy(receive_buffer_.begin(), receive_buffer_.begin() + bytes_transferred, body_.begin() + (body_.size() - body_to_receive_));
 			body_to_receive_ -= bytes_transferred;
 
-			// 바디를 다 채웠으면 다음 헤더 받을 준비
+			// 바디를 다 채웠으면 핸들러를 호출하고 다음 헤더 받을 준비
 			if (body_to_receive_ == 0)
 			{
-				std::string message(body_.begin(), body_.end());
-				size_t header;
-				std::memcpy(&header, header_.data(), sizeof(header_.data()));
-				std::cout << "[HEADER: BODY_LENGTH=" << header << "BYTES, (" << sizeof(header_.data()) << "bytes), BODY: MESSAGE=" << message << "(" << body_.size() << "bytes)]" << std::endl;
+				
+
+				OnPacketReceived();
+
 				header_to_receive_ = header_.size();
 				socket_.async_receive(boost::asio::buffer(receive_buffer_, header_to_receive_), boost::bind(&TcpConnection::ReceiveHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 			}
@@ -80,35 +104,87 @@ private:
 		}
 	}
 
-	void SendHandler(const boost::system::error_code& /*error*/, size_t /*bytes_transferred*/)
+	void OnPacketReceived()
 	{
-		
+		switch (current_operation_)
+		{
+		case Unauthorized:
+		{
+			std::string entered_key(body_.begin(), body_.end());
+			if (entered_key == passkey_)
+			{
+				std::array<char, 1> buf = { '1' };
+				socket_.async_send(boost::asio::buffer(buf),
+					[this](const boost::system::error_code& /*e*/, size_t /*bytes_transferred*/)
+					{
+						current_operation_ = EnteringNickname;
+					});
+			}
+			else
+			{
+				std::array<char, 1> buf = { '0' };
+				socket_.async_send(boost::asio::buffer(buf),
+					[this](const boost::system::error_code& /*e*/, size_t /*bytes_transferred*/)
+					{
+						std::cout << "인증 실패: " << endpoint_ << std::endl;
+					});
+
+			}
+			break;
+		}
+
+		case EnteringNickname:
+		{
+			std::string nickname(body_.begin(), body_.end());
+			nickname_ = nickname;
+			std::cout << "[유저 입장]: " << nickname << std::endl;
+			current_operation_ = Online;
+			break;
+		}
+
+		case Online:
+			std::string message(body_.begin(), body_.end());
+			OnClientChat_(nickname_, message);
+			break;
+		}
 	}
+
+	const std::string passkey_;
+	ClientOperation current_operation_;
 
 	boost::asio::ip::tcp::socket socket_;
 	boost::asio::ip::tcp::endpoint endpoint_;
-	std::string send_buffer_;
 
 	std::array<char, 64> receive_buffer_;
 	std::array<char, 8> header_;
 	size_t header_to_receive_ = 0;
 	std::vector<char> body_;
 	size_t body_to_receive_ = 0;
+
+	std::string nickname_;
+
+	std::function<void(const std::string&, const std::string&)> OnClientChat_;
+	std::function<void(const std::string&)> OnClientDisconnected_;
 };
 
 class ChatServer
 {
 public:
-	ChatServer(boost::asio::io_context& io_context) :
+	ChatServer(boost::asio::io_context& io_context, const std::string& ip, unsigned short port, const std::string& passkey) :
 		io_context_(io_context),
-		acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 31414))
+		acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(ip), port)),
+		passkey_(passkey)
 	{
 		WaitForAccept();
 	}
 private:
 	void WaitForAccept()
 	{
-		new_connection_ = std::make_unique<TcpConnection>(io_context_);
+		new_connection_ = std::make_shared<TcpConnection>(
+			io_context_,
+			passkey_,
+			[this](const std::string& nickname, const std::string& message) { OnClientChatHandler(nickname, message); },
+			[this](const std::string& nickname) { OnClientDisconnected(nickname); });
 		acceptor_.async_accept(new_connection_->Socket(), new_connection_->Endpoint(), boost::bind(&ChatServer::AcceptHandler, this, boost::asio::placeholders::error));
 	}
 
@@ -117,7 +193,8 @@ private:
 		if (!error)
 		{
 			new_connection_->StartReceive();
-			connections_.emplace_back(std::move(this->new_connection_));
+			connections_.push_back(new_connection_);
+			new_connection_.reset();
 			WaitForAccept();
 		}
 		else
@@ -127,19 +204,35 @@ private:
 		}
 	}
 
-	std::string message_;
-	std::vector<std::unique_ptr<TcpConnection>> connections_;
+	void OnClientChatHandler(const std::string& nickname, const std::string& chat)
+	{
+		std::cout << nickname << ": " << chat << std::endl;
+	}
+
+	void OnClientDisconnected(const std::string& nickname)
+	{
+		std::cout << "[연결 종료]: " << nickname << std::endl;
+		std::erase_if(connections_, [](std::shared_ptr<TcpConnection> con) { return con->IsDisconnected(); });
+	}
+
+	std::string passkey_;
+	std::shared_ptr<TcpConnection> new_connection_;
+	std::vector<std::shared_ptr<TcpConnection>> connections_;
 	boost::asio::io_context& io_context_;
 	boost::asio::ip::tcp::acceptor acceptor_;
-	std::unique_ptr<TcpConnection> new_connection_;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc != 4)
+	{
+		std::cout << "usage: server <ip> <port> <channel-passkey>" << std::endl;
+		return 1;
+	}
 	try
 	{
 		boost::asio::io_context io_context;
-		ChatServer chat_server(io_context);
+		ChatServer chat_server(io_context, std::string(argv[1]), std::atoi(argv[2]), std::string(argv[3]));
 		io_context.run();
 	}
 	catch (std::exception& e)
